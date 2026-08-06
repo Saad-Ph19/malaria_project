edf36a63899b92336b49606331ecd5c3d98bcbf0
Saad-Ph19 <saad72106@gmail.com>
2026-08-06 19:04:24 -0400

    Update malaria.py

--- tabs/malaria.py
+++ tabs/malaria.py
@@ -8,7 +8,7 @@ import glob
 import os
 
 # Bednet data
-bednet_files = glob.glob("Bednet_Data/*.xlsx")
+bednet_files = glob.glob("Bednet_Data/*.xls")
 
 bednet_df = pd.read_excel(
     bednet_files[0],
@@ -45,7 +45,7 @@ bednet_long.rename(
 
 
 # Malaria data
-files = glob.glob("Malaria_Data/*.xlsx")
+files = glob.glob("Malaria_Data/*.xls")
 df_list = []
 for file in files:
     temp = pd.read_excel(file, header=1)
